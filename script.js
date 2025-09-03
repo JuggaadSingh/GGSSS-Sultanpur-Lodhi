259eef7c06943b1b8157e69a82cde6e6f80149ab
JuggaadSingh <sandha222004@gmail.com>
2025-09-03 21:25:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMenu(){
+    const menu = document.getElementById("nav-links");
+
+    if(menu.style.display == "block"){
+        menu.style.display = "none";
+    }
+    else{
+        menu.style.display = "block";
+    }
+
+}
