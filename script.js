e7cc937e4420c11d65b4eb34943b1eecc271c978
JuggaadSingh <sandha222004@gmail.com>
2025-09-03 21:53:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function toggleMenu(){
     const menu = document.querySelector(".nav-links");
 
-    if(menu.style.display == "block"){
+    if(menu.style.display == "flex"){
         menu.style.display = "none";
     }
     else{
-        menu.style.display = "block";
+        menu.style.display = "flex";
     }
 
 }
